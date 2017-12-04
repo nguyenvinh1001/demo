fee9e68631ad9c32d0d85efa414691eec6d5b6ff
vinh.nguyenxuan <vinh.xuan.nguyen.1994@gmail.com>
2017-12-04 10:54:42 +0700

    using map and generic

--- src/queryKnex/select.ts
+++ src/queryKnex/select.ts
@@ -5,16 +5,20 @@ import {
   Request,
   Response,
   NextFunction
-} from "express";
-
+} from "express"
 
 
 const router: RequestHandler = (req, res, next) => {
-  knex("User").select()
+
+  knex("User").select().limit(10)
+    .map( (row: <T>(arg: T) => T) =>{
+      return row.name;
+    })
     .then(data => res.status(200).json({data}))
-    .catch(err => res.status(400).json({err: "Fail"}))
+    .catch(err => res.status(400).json({ err: "Fail" }))
 
 }
+
 export function showKnex(req: Request, res: Response, next: NextFunction) {
   router(req, res, next)
 }
